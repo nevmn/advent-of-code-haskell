767bc3b621b6a513eddcaf7d44157b1631e0d2cc
Maxim Neverov <nevmn21@gmail.com>
2020-09-01 15:01:50 +0600

    2015 day 1: function for count floor and step

--- src/AOC/AOC2015/Day1.hs
+++ src/AOC/AOC2015/Day1.hs
@@ -1,19 +1,18 @@
 module AOC.AOC2015.Day1 where
 
 part1 :: String -> Int
-part1 = foldl
-    (\floor direction -> case direction of
-        '(' -> succ floor
-        ')' -> pred floor
-        _   -> floor
-    )
-    0
+part1 = fst . last . floorWithStep
 
 part2 :: String -> Int
-part2 = snd . head . dropWhile (\(floor, number) -> floor /= -1) . scanl
+part2 =
+    snd . head . dropWhile (\(floor, number) -> floor /= -1) . floorWithStep
+
+floorWithStep :: String -> [(Int, Int)]
+floorWithStep = scanl
     (\(floor, number) direction -> case direction of
         '(' -> (succ floor, succ number)
         ')' -> (pred floor, succ number)
         _   -> (floor, number)
     )
     (0, 0)
+
